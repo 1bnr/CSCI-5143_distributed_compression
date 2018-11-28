bec2067704974fbbf929249f03aec00217eb61a5
Abner Holsinger <holsi006@umn.edu>
2018-11-28 14:43:01 -0600

    verifies acks, but still not reading or writing (one or both). no errors

--- main/i2c_master.c
+++ main/i2c_master.c
@@ -9,9 +9,6 @@
 //#include <stdlib.h>
 #include "i2c_master.h"
 
-#define FRAM_W    0xA1
-#define FRAM_R    0xA0
-
 #define F_SCL     100000UL // SCL frequency
 #define Prescaler 1
 #define TWBR_val  ((((F_CPU / F_SCL) / Prescaler) - 16) / 2)
@@ -43,8 +40,7 @@ uint8_t i2c_start(uint8_t address) {
     while (!(TWCR & (1 << TWINT)));
 
     // check value of TWI Status Register. Mask prescaler bits.
-
-    if ( ((TWSR & 0xF8) != TW_MT_SLA_ACK) && ((TWSR & 0xF8) != TW_MR_SLA_ACK) ) {
+      if ( ((TWSR & 0xF8) != TW_MT_SLA_ACK) && ((TWSR & 0xF8) != TW_MR_SLA_ACK) ) {
 			printf("Error: i2c_start failed acknowledment\r\n");
 			printf("TWSR & 0xF8 : %04x\r\nTW_MT_DATA_ACK : %04x\r\n",(TWSR & 0xF8), TW_MT_DATA_ACK );
 			return 1;
@@ -82,7 +78,8 @@ uint8_t i2c_start_wait(uint8_t address) {
             continue;
         }
 
-        if( (TWSR & 0xF8) != TW_MT_SLA_ACK) return 1;
+        if ( (TWSR & 0xF8) != TW_MT_SLA_ACK) return 1;
+
         break;
     }
     return 0;
@@ -97,7 +94,6 @@ uint8_t i2c_read_ack(void) {
     // return received data from TWDR
     return TWDR;
 }
-
 
 /*************************************************************************
    Send one byte to I2C device
@@ -116,9 +112,10 @@ uint8_t i2c_write(uint8_t data) {
 
     // check value of TWI Status Register. Mask prescaler bits
 
-        if( (TWSR & 0xF8) != TW_MT_DATA_ACK) {
-                printf("TWSR & 0xF8 : %04x\r\nTW_MT_DATA_ACK : %04x\r\n",(TWSR & 0xF8), TW_MT_DATA_ACK );
-                return 1;}
+    if ( (TWSR & 0xF8) != TW_MT_DATA_ACK) {
+        printf("TWSR & 0xF8 : %04x\r\nTW_MT_DATA_ACK : %04x\r\n", (TWSR & 0xF8), TW_MT_DATA_ACK);
+        return 1;
+    }
 
     return 0;
 }/* i2c_write */
@@ -162,14 +159,15 @@ uint8_t i2c_receive(uint8_t address, uint8_t *data, uint16_t length) {
 uint8_t i2c_writeReg(uint8_t devaddr, uint8_t regaddr, uint8_t *data, uint16_t length) {
     i2c_init();
 
-    if (i2c_start(devaddr)) {
+    if (i2c_start((devaddr<<1) | I2C_WRITE)) {
         printf("i2c_start error\r\n");
         return 1;
     }
+
     // wait for end of transmission
     while (!(TWCR & (1 << TWINT)) );
     // first write is the destinatino address
-	  i2c_write((regaddr<<1) | I2C_WRITE);
+    i2c_write(regaddr);
     printf("address sent\r\n");
 
     // now we can start writting data
@@ -194,15 +192,15 @@ uint8_t i2c_readReg(uint8_t devaddr, uint8_t regaddr, uint8_t *data, uint16_t le
     // start device connection
     i2c_init();
 
-    if (i2c_start(devaddr)) {
+    if (i2c_start((devaddr<<1)) | I2C_WRITE ) {
         printf("Error: readReg::start(devaddr)\r\n");
         return 1;
     }
 
     // write the source address
-    i2c_write((regaddr<<1) | I2C_READ);
+    i2c_write(regaddr);
 
-    if (i2c_start(devaddr)) {
+    if (i2c_start((devaddr<<1) | I2C_READ)) {
         printf("Error in readingReg::start(devaddr | 0x01)\r\n");
         return 1;
     }
@@ -217,6 +215,7 @@ uint8_t i2c_readReg(uint8_t devaddr, uint8_t regaddr, uint8_t *data, uint16_t le
 
     return 0;
 }
+
 /*************************************************************************
    Issues a repeated start condition and sends address and transfer direction
 
--- main/lufa.c
+++ main/lufa.c
@@ -16,12 +16,9 @@ R/r: read\r\n\
 W/w: write\r\n\
 S/s: Set address\r\n"
 
-#define FRAM_W 0xA1
-#define FRAM_R 0xA0
-
 //extern volatile uint8_t in_ui_mode;
 volatile uint8_t ui_stage = 0;
-volatile uint8_t mem_location = 0x50;
+volatile uint8_t mem_location = 0x0;
 void handleInput(char c) {
 // WARNING: This uses a busy-wait, thus will block main loop until done
     const int COMMAND_BUFF_LEN = 16;
@@ -105,11 +102,11 @@ void handleCommand(char *command) {
         case ('r'): {
             // READ COMMAND
             memset(&buffer, 0, 16);
-            if (i2c_readReg(MB85RC_SLAVE_ID, mem_location, buffer, 16))
+            if (i2c_readReg(MB85RC_DEFAULT_ADDRESS, mem_location, buffer, 16))
             printf("ERROR in read\r\n");
           else
             for (int i=0;i< 16; i++)
-            printf("%04x: %04x\r\n", (mem_location+i), buffer[i]);
+            printf("addr: 0x%04x-> %d\r\n", (mem_location+i), buffer[i]);
             break;
         }
 
@@ -118,7 +115,7 @@ void handleCommand(char *command) {
         case ('w'): {
             // WRITE COMMAND
             sprintf((char *)buffer, "hello world");
-                if (i2c_writeReg(MB85RC_SLAVE_ID, mem_location, buffer, 11))
+                if (i2c_writeReg(MB85RC_DEFAULT_ADDRESS, mem_location, buffer, 11))
                   printf("ERROR in write\r\n");
             break;
         }
